b6ab236326b1427af9a8aaeaeb69941e0c6558d9
Zanqueta <matheuszanqueta20@gmail.com>
2024-06-24 06:06:56 -0300

    Atualização do banco e do envio das imagens para a pasta certa

--- AppLoginAspCoreHL/GerenciadorArquivos/GerenciadorArquivo.cs
+++ AppLoginAspCoreHL/GerenciadorArquivos/GerenciadorArquivo.cs
@@ -5,14 +5,14 @@
         public static string CadastrarImagemProduto(IFormFile file)
         {
             var NomeArquivo = Path.GetFileName(file.FileName);
-            var Caminho = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images", NomeArquivo);
+            var Caminho = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/livro", NomeArquivo);
 
             using (var stream = new FileStream(Caminho, FileMode.Create))
             {
                 file.CopyTo(stream);
             }
 
-            return Path.Combine("/Images", NomeArquivo).Replace("\\", "/");
+            return Path.Combine("/img/livro", NomeArquivo).Replace("\\", "/");
         }
     }
 }
